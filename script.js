666c5b749119faaa1639495b0a0e2fa2b22d172c
VISHNUKR2201 <krvishnu144@gmail.com>
2025-08-07 11:52:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+//3Array Access
+
+//let fruits = ["apple", "banana", "mango", "grape", "kiwi"];
+//console.log('First fruit', fruits[0]);
+//console.log('Last fruit', fruits[fruits.length - 1]);
+//console.log('Total number of fruits:', fruits.length);
+//console.log('"mango" using index:', fruits[2]);
+
+
+
+//4. Object Practice
+//let a = {
+  //name: "Vishnu", age: 28,  grade: "A", isPassed: true 
+//}
+//console.log('Name:' ,a.name);
+//console.log('Age:' ,a.age);
+//console.log('Grade:' ,a.grade);
+//console.log('Passed:' ,a.isPassed);
+
+
+
+
+//5Console Methods
+
+//console.log("Name: Vishnu");
+//console.warn("age: 28");
+//console.error("place: palakkad");
+
+//Clear 
+//console.clear();
+
+
+
+//2. Using Prompt & Confirm
+
+let age = prompt("Enter your age");
+ a = ["Access Denied", "Access Granted"];
+ b = Number(age > 18);
+
+console.log(a[b]);
+
+
+
+
+var x = 5;
+let y = 10;
+
+{
+  var x = 20;
+  let y = 30;
+
+  console.log(x); // ?
+  console.log(y); // ?
+}
+
+console.log(x); // ?
+console.log(y); // ?
+
+
